b645fb7fd8eba0b0efaf5e76269c728bc7b6df27
allan201gf <allan201gf@gmail.com>
2022-02-21 18:57:54 -0300

    Initial commit

---
+++ src/main/java/br/com/allangf/ToDoWithUsersAPI/ToDoWithUsersApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.allangf.ToDoWithUsersAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoWithUsersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoWithUsersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/allangf/ToDoWithUsersAPI/ToDoWithUsersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.allangf.ToDoWithUsersAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoWithUsersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
